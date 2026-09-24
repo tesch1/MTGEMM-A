3cace418805233ed7c87c8822550b71e2b5b1ea2
michael tesch <tesch1@gmail.com>
2026-09-23 22:05:29 -0500

    Eigen branch pinned to ba12 (disjoint parts per SME unit, narrow-panel kernel)

--- third_party/build.sh
+++ third_party/build.sh
@@ -10,7 +10,7 @@ EIGEN_URL=https://gitlab.com/libeigen/eigen.git
 EIGEN_REV=ec8593a7dbbf45d370b8e4feda5de106706b01bc
 # Eigen with the SME backend work of merge requests !3164 and follow-ups (branch sme-phase4 on the fork).
 EIGEN_BRANCH_URL=https://gitlab.com/tesch1/eigen.git
-EIGEN_BRANCH_REV=224d71b92a6a4ec6968645618b5321e47e486182
+EIGEN_BRANCH_REV=ba12f5fbab99aff3c9a1d2b7d48eb0c1d7c414d0
 JOBS=$(sysctl -n hw.ncpu 2>/dev/null || nproc)
 mkdir -p src install
 
